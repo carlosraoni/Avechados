eb0985c589893c372542d01323adef49dd7065e1
Cesar Palomo <cesarpalomo@gmail.com>
2012-06-16 18:27:33 -0300

    Constantes para target resolutions para HTC e Galaxy5.

--- Car2d/src/com/car/main/AvechadosDesktopStarter.java
+++ Car2d/src/com/car/main/AvechadosDesktopStarter.java
@@ -1,9 +1,10 @@
 package com.car.main;
 
 import com.badlogic.gdx.backends.jogl.JoglApplication;
+import com.car.utils.Constants;
 
 public class AvechadosDesktopStarter {
 	public static void main(String [] args){
-		new JoglApplication(new AvechadosGame(), "Teste", 240, 320, false);		
+		new JoglApplication(new AvechadosGame(), "Teste", Constants.TARGET_RES_W, Constants.TARGET_RES_H, false);		
 	}
 }
