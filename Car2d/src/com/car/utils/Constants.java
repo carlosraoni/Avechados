d2efdacaeb8dd9bb586c957cdd925f8d877be1f2
carlosraoni <carlosraoni@gmail.com>
2012-07-12 09:49:48 -0300

    Limpeza da classe de constantes.

--- Car2d/src/com/car/utils/Constants.java
+++ Car2d/src/com/car/utils/Constants.java
@@ -3,20 +3,6 @@ package com.car.utils;
 import com.badlogic.gdx.math.Vector2;
 
 public interface Constants {
-	public static final int INITIAL_LIFE = 100;
-	public static final float MAP_BOUNDARY = 10.0f;
-	public static final int DEGREE_TURN = 5;
-	public static final int SLOW_DEGREE_TURN = 3;
-	
-	public static final int TIME = 1;
-	
-	public static final int INITIAL_MAX_SPEED_PLAYER = 70;
-	public static final int INITIAL_ACCELERATION_PLAYER = 1;
-	public static final int BRAKE_SPEED = 5;
-	public static final int SLOW_BRAKE_SPEED = 2;
-	
-	public static final float CONVERT_DELTA_TO_PIXELS = -(1.0f / 20.0f);
-
 	public static int UNFILLED_DISTANCE = -1;
 	
 	public static final String START_PLAYER_COLUMN_KEY = "startPlayerColumn";
